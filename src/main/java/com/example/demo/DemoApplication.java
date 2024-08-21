e661439e58ac641a2370cf1d02cdb813979919dd
Dónal Murtagh <donal.murtagh@clarusonesourcing.com>
2024-08-21 16:04:51 +0100

    add Error Prone/NullAway checks

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
+		new MissingNullableAnnotations(null);
 		SpringApplication.run(DemoApplication.class, args);
 	}
-
 }
---
+++ src/main/java/com/example/demo/MissingNullableAnnotations.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+public class MissingNullableAnnotations {
+
+    MissingNullableAnnotations(String value) {
+    }
+
+    String doIt() {
+        return null;
+    }
+}
