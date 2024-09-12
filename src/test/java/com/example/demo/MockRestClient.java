5f8dcd69d971ec90eeaad2cd6a594aa9914f9008
Dónal Murtagh <donal.murtagh@clarusonesourcing.com>
2024-09-12 12:13:40 +0100

    define test port in property file

--- src/test/java/com/example/demo/MockRestClient.java
+++ src/test/java/com/example/demo/MockRestClient.java
@@ -1,10 +1,11 @@
 package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.client.JdkClientHttpRequestFactory;
@@ -12,10 +13,14 @@ import org.springframework.web.client.RestClient;
 
 @Lazy
 @TestConfiguration
+@PropertySource("classpath:test.properties")
 class MockRestClient {
 
+    @Value("${server.port}")
+    private String port;
+
     @Bean
-    RestClient restClient(@LocalServerPort int port, @Autowired RestClient.Builder restClientBuilder) {
+    RestClient restClient(@Autowired RestClient.Builder restClientBuilder) {
         return restClientBuilder.baseUrl("http://localhost:" + port)
             // We need to change the default request factory in order to use PATCH requests
             // https://github.com/spring-projects/spring-framework/issues/31590
