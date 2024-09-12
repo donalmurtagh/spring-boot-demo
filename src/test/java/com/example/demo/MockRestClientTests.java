4bf220b571bac3a1670deb95e3f9534eb14055de
Dónal Murtagh <donal.murtagh@clarusonesourcing.com>
2024-09-12 14:12:49 +0100

    move @TestConfiguration classes into the same file as the tests that use them

--- src/test/java/com/example/demo/MockRestClientTests.java
+++ src/test/java/com/example/demo/MockRestClientTests.java
@@ -2,9 +2,16 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.JdkClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -27,3 +34,17 @@ class MockRestClientTests {
 	}
 }
 
+@Lazy
+@TestConfiguration
+class MockRestClient {
+
+    @Bean
+    RestClient restClient(@Value("${server.port}") int port, @Autowired RestClient.Builder restClientBuilder) {
+        return restClientBuilder.baseUrl("http://localhost:" + port)
+            // We need to change the default request factory in order to use PATCH requests
+            // https://github.com/spring-projects/spring-framework/issues/31590
+            .requestFactory(new JdkClientHttpRequestFactory())
+            .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+            .build();
+    }
+}
--- src/test/java/com/example/demo/RandomPortTests.java
+++ src/test/java/com/example/demo/RandomPortTests.java
@@ -3,7 +3,14 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.JdkClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -25,3 +32,17 @@ class RandomPortTests {
 	}
 }
 
+@Lazy
+@TestConfiguration
+class RandomPortRestClient {
+
+    @Bean
+    RestClient restClient(@LocalServerPort int port, @Autowired RestClient.Builder restClientBuilder) {
+        return restClientBuilder.baseUrl("http://localhost:" + port)
+            // We need to change the default request factory in order to use PATCH requests
+            // https://github.com/spring-projects/spring-framework/issues/31590
+            .requestFactory(new JdkClientHttpRequestFactory())
+            .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+            .build();
+    }
+}
