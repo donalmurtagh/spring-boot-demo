ed2fb44f744c04aed09d0847f0cfb1be9bc39357
Dónal Murtagh <donal.murtagh@clarusonesourcing.com>
2025-10-28 15:43:14 +0000

    remove annotation

--- src/main/java/com/example/demo/ItemController.java
+++ src/main/java/com/example/demo/ItemController.java
@@ -1,13 +1,11 @@
 package com.example.demo;
 
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/item")
-@PreAuthorize("hasAuthority('FOO')")
 public class ItemController {
 
     @GetMapping
