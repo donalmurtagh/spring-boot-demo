a26b3b50649d7e3e38eecd9bd0108a6a3b527309
Dónal Murtagh <donal.murtagh@clarusonesourcing.com>
2025-10-29 13:38:18 +0000

    remove irrelevant stuff

--- src/test/java/com/example/demo/RestSpringBeans.java
+++ src/test/java/com/example/demo/RestSpringBeans.java
@@ -2,10 +2,6 @@ package com.example.demo;
 
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Lazy;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.http.converter.json.JacksonJsonHttpMessageConverter;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.client.RestTestClient;
 
@@ -14,8 +10,6 @@ public class RestSpringBeans {
 
     @Bean
     RestTestClient restTestClient(MockMvc mockMvc) {
-        return RestTestClient.bindTo(mockMvc)
-            .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-            .build();
+        return RestTestClient.bindTo(mockMvc).build();
     }
 }
